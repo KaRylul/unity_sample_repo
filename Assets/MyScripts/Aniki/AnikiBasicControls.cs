f44b1aee164fa001ab239f470a4b5188ace1ae32
KaRylul <KaRylul@yandex.by>
2019-01-02 01:47:01 +0300

    Added basic anim, still need to fix rotations (prob some anim causes it to fuck up)

---
+++ Assets/MyScripts/Aniki/AnikiBasicControls.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnikiBasicControls : MonoBehaviour
+{
+    [SerializeField]
+    private float walkSpeed = 2f;
+    [SerializeField]
+    private float runSpeed = 4f;
+    [SerializeField]
+    private float speedSmoothVelocity = 0.01f;
+    [SerializeField]
+    private float speedSmoothTime = 0.01f;
+    [SerializeField]
+    private float turnSmoothVelocity = 0.01f;
+    [SerializeField]
+    private float turnSmoothTime = 0.04f;
+    [SerializeField]
+    private float jumpForce = 5.2f;
+
+    [SerializeField]
+    private Transform playerCamera;
+
+    private float currentSpeed;
+    private float velocityY;
+    private float gravity = 15f;
+    private CharacterController controller;
+    private Animator animator;
+    private float forwardPercent;
+    private bool isRunning;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        animator = GetComponentInChildren<Animator>();
+    }
+    
+    private void Update()
+    {
+        // input
+        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        Vector3 inputDir = input.normalized;
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            MovePlayer(inputDir, true);
+        }
+        else
+        {
+            MovePlayer(inputDir, false);
+        }
+
+
+    }
+
+    private void MovePlayer(Vector3 inputDir, bool running)
+    {
+        #region Movement raw script
+        if (inputDir != Vector3.zero)
+        {
+            // this is used to rotate player relatively to camera rotation
+            float targetRotation = Mathf.Atan2(inputDir.x, inputDir.z) * Mathf.Rad2Deg + playerCamera.eulerAngles.y;
+            //transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, turnSmoothTime);
+            transform.eulerAngles = Vector3.up * targetRotation;
+        }
+
+        float targetSpeed = walkSpeed;
+        targetSpeed = ((running) ? runSpeed : walkSpeed) * inputDir.magnitude;
+
+        currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedSmoothVelocity, speedSmoothTime);
+
+        #region Jump mechanics
+        if (controller.isGrounded)
+        {
+            velocityY = -gravity * Time.deltaTime;
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                velocityY = jumpForce;
+            }
+        }
+        else
+        {
+            velocityY -= gravity * Time.deltaTime;
+        }
+        #endregion
+
+        Vector3 velocity = transform.forward * currentSpeed + Vector3.up * velocityY;
+        controller.Move(velocity * Time.deltaTime);
+        #endregion
+
+        #region Animation
+        forwardPercent = currentSpeed;
+        Debug.Log(forwardPercent);
+        animator.SetFloat("forwardPercent", currentSpeed);
+
+
+        #endregion
+    }
+
+
+
+}
---
+++ Assets/MyScripts/Aniki/MyCam.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyCam : MonoBehaviour {
+
+    [SerializeField]
+    private float mouseSensitivity;
+    [SerializeField]
+    private Transform target;
+    [SerializeField]
+    private float dstFromTarget;
+    [SerializeField]
+    private Animator characterAnimator;
+    
+    private Vector2 pitchMinMax = new Vector2(-40, 85);    
+    private float rotationSmoothTime;
+    private Vector3 targetPosAiming = new Vector3(0.511f, 1.387f, 0.598f);
+    private Vector3 targetPosDefault;
+    //time to take from start to end
+    private float lerpTime = 0.3f;
+    //this will update the lerp time
+    private float currentLerpTime = 0;
+    Vector3 rotationSmoothVelocity;
+    Vector3 currentRotation;
+
+    float yaw;
+    float pitch;
+
+    void Start()
+    {
+        rotationSmoothTime = 0.05f;
+        dstFromTarget = 1.2f;
+        mouseSensitivity = 8f;
+        targetPosDefault = target.localPosition;
+    }
+
+    void LateUpdate()
+    {
+        yaw += Input.GetAxis("Mouse X") * mouseSensitivity;
+        pitch -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+        pitch = Mathf.Clamp(pitch, pitchMinMax.x, pitchMinMax.y);
+        currentRotation = Vector3.SmoothDamp(currentRotation, new Vector3(pitch, yaw), ref rotationSmoothVelocity, rotationSmoothTime);
+
+        transform.eulerAngles = currentRotation;
+        transform.position = target.position - transform.forward * dstFromTarget;
+
+        if(Input.GetMouseButton(1))
+        {
+            Aiming(targetPosDefault);
+            pitchMinMax = new Vector2(-30, 50);
+        }
+        else
+        {
+            pitchMinMax = new Vector2(-40, 85);
+            currentLerpTime -= Time.deltaTime;
+            if (currentLerpTime <= 0)
+            {
+                currentLerpTime = 0;
+            }
+            float lerpPerc = currentLerpTime / lerpTime;
+            target.localPosition = Vector3.Lerp(targetPosDefault, targetPosAiming, lerpPerc);
+        }
+
+    }
+
+    private void Aiming(Vector3 targetPosDefault)
+    {
+        currentLerpTime += Time.deltaTime;
+        if(currentLerpTime >= lerpTime)
+        {
+            currentLerpTime = lerpTime;
+        }
+        float lerpPerc = currentLerpTime / lerpTime;
+        target.localPosition = Vector3.Lerp(targetPosDefault, targetPosAiming, lerpPerc);
+
+        //animation
+        float animationSpeedPercent = currentRotation.x;
+        characterAnimator.SetFloat("aimingPercent", animationSpeedPercent, 0.01f, Time.deltaTime);
+    }
+
+
+}
